62303c3a7c73f966c4a8e0a3e5b0d22580595edb
Chunmun-18 <chunmungadodia@gmail.com>
2025-09-26 23:01:15 +0530

    Events updated

--- src/components/Eventsinfo.js
+++ src/components/Eventsinfo.js
@@ -6,7 +6,7 @@ function Eventsinfo() {
   return (
     <div className='einfo-container'>
         <div className="einfo-parent">
-            <h2>Events</h2>
+            <h2 className="events">Events</h2>
             <Ebox
             heading = "Box Office"
             para = "Cinema Club IITR, in collaboration with Thomso, proudly hosted Box Office – the ultimate movie quiz celebrating cinema in all its forms. From timeless classics to modern blockbusters, participants put their film knowledge to the test in an exciting and competitive atmosphere."
@@ -16,6 +16,11 @@ function Eventsinfo() {
             heading="Beyond the Lens"
             para="Cinema Club proudly hosted Beyond the Lens —a creative competition that celebrated the art of storytelling through engaging write-ups and innovative video editing. Participants explored unique cinematic perspectives, showcasing their originality, imagination, and editing skills."
             image="./images/btlens.jpg"
+            />
+            <Ebox
+            heading="False Script Reel Competition"
+            para="Cinema Club proudly hosted the False Script Reel Competition — a quirky challenge where participants reimagined iconic movie scenes with humorous twists, dramatic dialogues, and over-the-top creativity. From genre remixes to filmy masala, the event showcased originality, wit, and a fresh take on cinema."
+            image="./images/FalseScript.jpeg"
             />
         </div>
       
